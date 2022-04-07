efe1a96ff01c3739efc970229c7081b768ce4302
kim1387 <kevink0@naver.com>
2022-04-07 15:16:31 +0900

    feat: 안쓰는 기본 옵션들 비활성화
    - 세션 비활성화
    - form login과 httpbasic 비활성화
    - csrf 비활성화

--- backend/src/main/java/com/hjjang/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/hjjang/backend/global/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.hjjang.backend.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -17,5 +19,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         web.ignoring().antMatchers(
                 "/v2/api-docs", "/configuration/ui", "/swagger-resources",
                 "/configuration/security", "/swagger-ui.html", "/webjars/**", "/swagger/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // spring security 기본 로그인 옵션을 비활성화 환다.
+        http
+                    .csrf().disable()  // csrf은 세션 로그인용 보안 설정
+                    .httpBasic().disable()
+                    .formLogin().disable()
+                    .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS); // jwt는 세션을 사용하지 않는다.
+
+
     }
 }

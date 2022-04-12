33bdf21aba8f0624df48f7a5934356d183f4dd87
kim1387 <kevink0@naver.com>
2022-04-12 22:49:14 +0900

    fix: rename RefreshToken entity and repository, add Builder and setter for setRefreshtoken
    - user의 토큰 엔티티 이름 바꿈
    - 사용하는 method들 추가

--- backend/src/main/java/com/hjjang/backend/domain/user/repository/RefreshTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hjjang.backend.domain.user.repository;
-
-import com.hjjang.backend.domain.user.entity.RefreshToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken,Long> {
-
-}

e2f97c1d918ae3744d6815801e99e003f3aa3463
kim1387 <kevink0@naver.com>
2022-04-12 22:53:24 +0900

    feat: modify some columns name and attributes, add builder constructor

--- backend/src/main/java/com/hjjang/backend/domain/user/entity/User.java
+++ backend/src/main/java/com/hjjang/backend/domain/user/entity/User.java
@@ -1,38 +1,41 @@
 package com.hjjang.backend.domain.user.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
-
 import java.time.LocalDateTime;
 
 import static javax.persistence.EnumType.STRING;
-import static javax.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
+@Getter
+@AllArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
 @Entity
 @NoArgsConstructor(access = PROTECTED)
 @Table(name = "user")
 public class User {
 
-    @Id @GeneratedValue(strategy = IDENTITY)
-    @Column(name = "id", nullable = false)
-    private Long id;
+    @Id
+    @Column(name = "id", nullable = false, length = 128)
+    private String id;
 
     @Column(name = "nickname", nullable = false, length = 30)
     private String nickName;
 
-    @Column(name = "email", nullable = false, length = 50)
+    @Column(name = "email", nullable = true, length = 50)
     private String email;
 
-    @Column(name = "manner_temperature", nullable = true)
+    @Column(name = "manner_temperature", nullable = false)
     private Long mannerTemperature;
 
-    @Column(name = "img_url", nullable = true, length = 50)
-    private String imgURL;
+    @Column(name = "image_url", nullable = true, length = 50)
+    private String imageUrl;
 
     @CreatedDate
     @Column(name = "created_at", nullable = false)
@@ -45,7 +48,19 @@ public class User {
     @Column(name = "univ_id")
     private Long univId;
 
-    @Column(name = "role")
-    @Enumerated(EnumType.STRING)
-    private UserRole role;
+    @Column(name = "role",length = 20)
+    @Enumerated(STRING)
+    private RoleType role;
+
+    @Builder
+    public User(String id, String nickName, String email, Long mannerTemperature, String imageUrl, Agreement isPushAgree, Long univId, RoleType role) {
+        this.id = id;
+        this.nickName = nickName;
+        this.email = email;
+        this.mannerTemperature = mannerTemperature;
+        this.imageUrl = imageUrl;
+        this.isPushAgree = isPushAgree;
+        this.univId = univId;
+        this.role = role;
+    }
 }

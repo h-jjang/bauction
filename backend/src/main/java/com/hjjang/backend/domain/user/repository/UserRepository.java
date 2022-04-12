4a8e70ba32c74c1ccf3770ec33ac7f94055c2a25
kim1387 <kevink0@naver.com>
2022-04-12 22:50:02 +0900

    feat: add findUserById for return not optional class

--- backend/src/main/java/com/hjjang/backend/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/hjjang/backend/domain/user/repository/UserRepository.java
@@ -3,6 +3,6 @@ package com.hjjang.backend.domain.user.repository;
 import com.hjjang.backend.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-
+public interface UserRepository extends JpaRepository<User, String> {
+    User findUserById(String Id);
 }

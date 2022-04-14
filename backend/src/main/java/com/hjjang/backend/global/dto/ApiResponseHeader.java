ed3193fadbe8f9eaa7932d9575ffcdb920c5da78
kim1387 <kevink0@naver.com>
2022-04-14 14:08:44 +0900

    feat: api 응답을 위한 임시적인 조치 나중에 따로 만들 필요가 있다.

---
+++ backend/src/main/java/com/hjjang/backend/global/dto/ApiResponseHeader.java
@@ -0,0 +1,13 @@
+package com.hjjang.backend.global.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class ApiResponseHeader {
+    private int code;
+    private String message;
+}

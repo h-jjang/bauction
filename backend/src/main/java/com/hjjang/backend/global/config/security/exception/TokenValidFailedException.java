9ca958fec53212d3cdf58adaea94f0fbc89f1389
kim1387 <kevink0@naver.com>
2022-04-14 14:34:17 +0900

    feat: 유효하지 않을 경우 예외 클래스

---
+++ backend/src/main/java/com/hjjang/backend/global/config/security/exception/TokenValidFailedException.java
@@ -0,0 +1,12 @@
+package com.hjjang.backend.global.config.security.exception;
+
+public class TokenValidFailedException extends RuntimeException {
+
+    public TokenValidFailedException() {
+        super("Failed to generate Token.");
+    }
+
+    private TokenValidFailedException(String message) {
+        super(message);
+    }
+}

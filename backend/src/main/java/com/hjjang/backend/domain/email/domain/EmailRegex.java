a64ddbdf2276868256bef6023d3e47de782181d8
Yunkeun <yunx2@naver.com>
2022-04-17 04:17:06 +0900

    fix: valid 이메일 관련 로직 수정 @gs.anyang.ac.kr 과 같이 gs.으로 시작하는 이메일도 가능하게 수정한다.

--- backend/src/main/java/com/hjjang/backend/domain/email/domain/EmailRegex.java
+++ backend/src/main/java/com/hjjang/backend/domain/email/domain/EmailRegex.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum EmailRegex {
-	UNIVERSITY("UNIVERSITY_EMAIL_REGEX", "^[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.+a+c+\\.+k+r+$"),
+	UNIVERSITY("UNIVERSITY_EMAIL_REGEX", "^[a-zA-Z0-9]+@[a-zA-Z0-9.]+\\.+a+c+\\.+k+r+$"),
 	EMAIL_PARSE("EMAIL_PARSE_REGEX", "[@.]");
 
 	private final String name;

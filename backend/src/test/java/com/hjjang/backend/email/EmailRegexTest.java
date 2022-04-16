3df45dc8b603f4f3ad054e3e96d697f8df736d8a
Yunkeun <yunx2@naver.com>
2022-04-17 04:14:56 +0900

    test: 이메일 인증 관련 regex와 parse 테스트

--- backend/src/test/java/com/hjjang/backend/email/EmailRegexTest.java
+++ backend/src/test/java/com/hjjang/backend/email/EmailRegexTest.java
@@ -2,27 +2,29 @@ package com.hjjang.backend.email;
 
 import static org.assertj.core.api.Assertions.*;
 
-import com.hjjang.backend.domain.email.exception.MailException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class EmailRegexTest {
 
-	private static final String REGEX = "^[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.+a+c+\\.+k+r+$";
+	private static final String REGEX = "^[a-zA-Z0-9]+@[a-zA-Z0-9.]+\\.+a+c+\\.+k+r+$";
 	private static final String EMAIL_PARSE_REGEX = "[@.]";
 
 	@Test
 	void parseUniversity() {
 		//given
 		String email = "32174294@dankook.ac.kr";
+		String email2 = "dr0joon@gs.anyang.ac.kr";
 		//when
-		String university = List.of(email.split(EMAIL_PARSE_REGEX)).get(1);
+		List<String> split = List.of(email.split(EMAIL_PARSE_REGEX));
+		List<String> split2 = List.of(email2.split(EMAIL_PARSE_REGEX));
+		int univIndex = split.lastIndexOf("ac") - 1;
+		int univIndex2 = split2.lastIndexOf("ac") - 1;
 		//then
-		System.out.println("university = " + university);
-		assertThat(university).isEqualTo("dankook");
+		assertThat(split.get(univIndex)).isEqualTo("dankook");
+		assertThat(split2.get(univIndex2)).isEqualTo("anyang");
 	}
 
 	@Test
@@ -52,6 +54,7 @@ class EmailRegexTest {
 		trueEmails.add("32174294@dankook.ac.kr");
 		trueEmails.add("32174294@anyang.ac.kr");
 		trueEmails.add("32174294@kpu.ac.kr");
+		trueEmails.add("dr0joon@gs.anyang.ac.kr");
 		//then
 		trueEmails.forEach(email -> {
 			boolean match = Pattern.matches(REGEX, email);

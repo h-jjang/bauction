8c08c4b3b358488e03186ca6621f8ddf25cccbeb
0BVer <dr0joon@gmail.com>
2022-04-19 21:29:54 +0900

    feat: change image save path

--- backend/src/main/java/com/hjjang/backend/domain/image/component/LocalUploader.java
+++ backend/src/main/java/com/hjjang/backend/domain/image/component/LocalUploader.java
@@ -33,7 +33,7 @@ public class LocalUploader implements ImageUploader {
     // 로컬에 파일 업로드 하기
     @Override
     public Optional<File> localUpload(MultipartFile file) throws IOException {
-        File convertFile = new File(System.getProperty("user.dir") + changeFileName(file));
+        File convertFile = new File(System.getProperty("user.dir") + "/backend/src/main/resources/images/" + changeFileName(file));
         if (convertFile.createNewFile()) { // 바로 위에서 지정한 경로에 File이 생성됨 (경로가 잘못되었다면 생성 불가능)
             try (FileOutputStream fos =
                          new FileOutputStream(convertFile)) { // FileOutputStream 데이터를 파일에 바이트 스트림으로 저장하기 위함

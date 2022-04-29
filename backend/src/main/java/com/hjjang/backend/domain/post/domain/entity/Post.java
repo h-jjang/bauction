85085b196372d5f408e3b730b2c1ee2ca7c5cf47
0BVer <dr0joon@gmail.com>
2022-04-30 02:33:12 +0900

    debug : 생성 조회 디버그

--- backend/src/main/java/com/hjjang/backend/domain/post/domain/entity/Post.java
+++ backend/src/main/java/com/hjjang/backend/domain/post/domain/entity/Post.java
@@ -3,6 +3,8 @@ package com.hjjang.backend.domain.post.domain.entity;
 import com.hjjang.backend.domain.user.entity.User;
 import com.hjjang.backend.infra.image.domain.entity.Image;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
 
 import javax.persistence.*;
 
@@ -10,20 +12,21 @@ import javax.persistence.*;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicInsert
 @Table(schema = "post")
 @Entity
 public class Post {
     @Id
-    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    private User user;
+//    @ManyToOne
+//    @JoinColumn(name = "user_id")
+//    private User user;
 
-    @ManyToOne
-    @JoinColumn(name = "image_id")
-    private Image image;
+//    @ManyToOne
+//    @JoinColumn(name = "image_id")
+//    private Image image;
 
     private String title;
 
@@ -31,14 +34,19 @@ public class Post {
 
     private Integer item_price;
 
+    @ColumnDefault("0")
     private Integer views = 0;
 
+    @ColumnDefault("0")
     private Integer interest_number = 0;
 
+    @ColumnDefault("0")
     private Integer chat_number = 0;
 
-    private String is_sale_completion;
+    @ColumnDefault("'false'")
+    private String is_sale_completion = "false";
 
+    @ColumnDefault("false")
     private boolean removed = false;
 
     public void removePost() {

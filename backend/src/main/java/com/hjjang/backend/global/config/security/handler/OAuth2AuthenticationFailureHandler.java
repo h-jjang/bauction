1b3ccab5aca0a6f058f18ba5fcb0e5a1e3a6d5b5
kim1387 <kevink0@naver.com>
2022-05-03 18:01:27 +0900

    style: sebatch package importing

--- backend/src/main/java/com/hjjang/backend/global/config/security/handler/OAuth2AuthenticationFailureHandler.java
+++ backend/src/main/java/com/hjjang/backend/global/config/security/handler/OAuth2AuthenticationFailureHandler.java
@@ -1,23 +1,20 @@
 package com.hjjang.backend.global.config.security.handler;
 
-import static com.hjjang.backend.global.config.security.repository.OAuth2AuthorizationRequestBasedOnCookieRepository.*;
-
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.hjjang.backend.global.config.security.repository.OAuth2AuthorizationRequestBasedOnCookieRepository;
+import com.hjjang.backend.global.util.CookieUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import com.hjjang.backend.global.config.security.repository.OAuth2AuthorizationRequestBasedOnCookieRepository;
-import com.hjjang.backend.global.util.CookieUtil;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-import lombok.RequiredArgsConstructor;
+import static com.hjjang.backend.global.config.security.repository.OAuth2AuthorizationRequestBasedOnCookieRepository.REDIRECT_URI_PARAM_COOKIE_NAME;
 
 @Component
 @RequiredArgsConstructor

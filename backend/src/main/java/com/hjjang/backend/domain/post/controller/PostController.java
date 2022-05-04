7879802a43c7aa27e6a38557a8d5f622c8059d25
0BVer <dr0joon@gmail.com>
2022-05-04 23:51:27 +0900

    feat : 기존 api에 ApiResponse 적용

--- backend/src/main/java/com/hjjang/backend/domain/post/controller/PostController.java
+++ backend/src/main/java/com/hjjang/backend/domain/post/controller/PostController.java
@@ -3,6 +3,8 @@ package com.hjjang.backend.domain.post.controller;
 import com.hjjang.backend.domain.post.dto.PostRequest;
 import com.hjjang.backend.domain.post.dto.PostResponse;
 import com.hjjang.backend.domain.post.service.PostServiceImpl;
+import com.hjjang.backend.global.dto.ApiResponse;
+import com.hjjang.backend.global.util.UserUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,37 +20,44 @@ import java.util.stream.Collectors;
 public class PostController {
 
     private final PostServiceImpl postService;
+    private final UserUtil userUtil;
 
     @PostMapping
-    public ResponseEntity<PostResponse> createItem(@Validated @RequestBody PostRequest postRequest) {
+    public ResponseEntity<ApiResponse> createItem(@Validated @RequestBody PostRequest postRequest) {
         return ResponseEntity.ok(
-                PostResponse.of(
-                        postService.save(
-                                postRequest.toEntity()
+                ApiResponse.success("createItem",
+                        PostResponse.of(
+                                postService.save(
+                                        postRequest.toEntity(
+                                                userUtil.getLoginUserByToken()
+                                        )
+                                )
                         )
                 )
         );
     }
 
     @GetMapping
-    public ResponseEntity<List<PostResponse>> findAllItem() {
-        return ResponseEntity.ok(postService
-                .findAll()
-                .stream()
-                .map(PostResponse::of)
-                .collect(Collectors.toList())
+    public ResponseEntity<ApiResponse> findAllItem() {
+        return ResponseEntity.ok(
+                ApiResponse.success("findAllItem", postService
+                        .findAll()
+                        .stream()
+                        .map(PostResponse::of)
+                        .collect(Collectors.toList())
+                )
         );
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<PostResponse> findOneItem(@PathVariable Long id) {
-        return ResponseEntity.ok(PostResponse.of(postService.findOneById(id)));
+    public ResponseEntity<ApiResponse> findOneItem(@PathVariable Long id) {
+        return ResponseEntity.ok(ApiResponse.success("findOneItem", PostResponse.of(postService.findOneById(id))));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<HttpStatus> deleteOneItem(@PathVariable Long id) {
+    public ResponseEntity<ApiResponse> deleteOneItem(@PathVariable Long id) {
         postService.deleteOneById(id);
-        return ResponseEntity.ok(HttpStatus.ACCEPTED);
+        return ResponseEntity.ok(ApiResponse.success("deleteOneItem", HttpStatus.ACCEPTED));
     }
 
 

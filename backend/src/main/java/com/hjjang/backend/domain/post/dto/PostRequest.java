c6f5b53592a63cee0be07b9b07122ac48b621ff9
0BVer <dr0joon@gmail.com>
2022-05-06 00:14:48 +0900

    debug : requestDto 버그 수정

--- backend/src/main/java/com/hjjang/backend/domain/post/dto/PostRequest.java
+++ backend/src/main/java/com/hjjang/backend/domain/post/dto/PostRequest.java
@@ -5,19 +5,20 @@ import com.hjjang.backend.domain.user.entity.User;
 import lombok.*;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @RequiredArgsConstructor
 public class PostRequest {
-    @NotEmpty
+    @NotNull
     private String title;
 
-    @NotEmpty
+    @NotNull
     private String content;
 
-    @NotEmpty
+    @NotNull
     private int price;
 
     public Post toEntity(User user) {

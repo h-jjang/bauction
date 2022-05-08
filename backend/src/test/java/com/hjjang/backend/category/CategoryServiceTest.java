56e418d91c536eb70956bce5401f6ca882567003
Gnu-Kenny <qkr0677@gmail.com>
2022-05-08 14:43:18 +0900

    feat: Mockito를 이용한 카테고리 전체 조회 테스트 코드 작성

--- backend/src/test/java/com/hjjang/backend/category/CategoryServiceTest.java
+++ backend/src/test/java/com/hjjang/backend/category/CategoryServiceTest.java
@@ -2,13 +2,25 @@ package com.hjjang.backend.category;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.hjjang.backend.domain.category.domain.entity.Category;
+import com.hjjang.backend.domain.category.domain.repository.CategoryRepository;
 import com.hjjang.backend.domain.category.dto.CategoryRequest;
+import com.hjjang.backend.domain.category.service.CategoryService;
 
+@ExtendWith(SpringExtension.class)
 class CategoryServiceTest {
+	@Mock
+	private CategoryService categoryService;
+	@Mock
+	private CategoryRepository categoryRepository;
 
 	@DisplayName("카테고리 등록 기능")
 	@Test
@@ -23,5 +35,19 @@ class CategoryServiceTest {
 		assertThat(category.getName()).isEqualTo(categoryRequest.getName());
 		assertThat(category.getRemoved()).isEqualTo(false);
 	}
+	@DisplayName("카테고리 전체 조회 기능")
+	@Test
+	void 카테고리_전체_조회(){
+		//given
+		String name = "가전제품";
+		CategoryRequest categoryRequest = new CategoryRequest(name);
+		Category category = categoryRequest.toEntity();
+		categoryRepository.save(category);
 
+		//when
+		List<Category> categoryList = categoryService.findAll();
+		//then
+		List<Category> categoryListRepository = categoryRepository.findAll();
+		assertThat(categoryList).isEqualTo(categoryListRepository);
+	}
 }

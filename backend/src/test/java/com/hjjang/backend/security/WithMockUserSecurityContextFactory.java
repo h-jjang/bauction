2729e278b052bcdfe83d121c235652a0a00ee006
kim1387 <kevink0@naver.com>
2022-05-14 02:11:08 +0900

    fix: 가짜 사용자와 가짜 사용자와 인증 정보 추가하는 어노테이션 완성

--- backend/src/test/java/com/hjjang/backend/security/WithMockUserSecurityContextFactory.java
+++ backend/src/test/java/com/hjjang/backend/security/WithMockUserSecurityContextFactory.java
@@ -1,32 +1,45 @@
 package com.hjjang.backend.security;
 
-import com.hjjang.backend.domain.user.entity.User;
-import com.hjjang.backend.global.security.principal.UserPrincipal;
+import com.hjjang.backend.domain.user.entity.RoleType;
+import com.hjjang.backend.global.config.security.properties.AuthProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.test.context.support.WithSecurityContextFactory;
 
+import java.util.Collections;
+import java.util.Date;
+
+
+@RequiredArgsConstructor
 public class WithMockUserSecurityContextFactory implements WithSecurityContextFactory<WithMockCustomUser> {
+
+    private final AuthProperties authProperties;
+
     @Override
     public SecurityContext createSecurityContext(WithMockCustomUser customUser) {
-        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
 
-        User givenUser = User.builder()
-                .email(customUser.email())
-                .imageUrl(customUser.imageUrl())
-                .isPushAgree(customUser.isPushAgree())
-                .mannerTemperature(customUser.mannerTemperature())
-                .nickName(customUser.nickName())
-                .providerId(customUser.providerId())
-                .role(customUser.roles())
-                .univId(customUser.univId())
-                .build();
-            
-        UserPrincipal principal = UserPrincipal.create(givenUser);
-        Authentication auth = new UsernamePasswordAuthenticationToken(principal, "", principal.getAuthorities());
-        context.setAuthentication(auth);
-        return context;
+        Date now = new Date();
+
+        // 가짜 user 정보 설정
+        User principal = new User(
+                customUser.providerId(),
+                "",
+                Collections.singletonList(new SimpleGrantedAuthority(RoleType.USER.getCode()))
+        );
+
+        // 가짜 security 인증 user 저장
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                principal,
+                "",
+                principal.getAuthorities()
+        );
+        securityContext.setAuthentication(authentication);
+        return securityContext;
     }
 }

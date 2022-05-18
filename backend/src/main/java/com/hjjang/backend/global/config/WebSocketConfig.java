d3996d52265881101cd5a343feaa0546496fb475
kim1387 <kevink0@naver.com>
2022-05-19 01:04:08 +0900

    feat: add stomp config

---
+++ backend/src/main/java/com/hjjang/backend/global/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.hjjang.backend.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/secured/user/queue/specific-user");
+        config.setApplicationDestinationPrefixes("/spring-security-mvc-socket");
+        config.setUserDestinationPrefix("/secured/user");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry config) {
+        config.addEndpoint("/secured/room")
+                .withSockJS();
+    }
+}

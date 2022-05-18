5e69a4237764060d9a9bc06a9134cf60832a6c01
kim1387 <kevink0@naver.com>
2022-05-19 00:52:10 +0900

    feat: 이미지 message를 위한 상속 message entity

---
+++ backend/src/main/java/com/hjjang/backend/domain/chat/domain/ChatMessageImage.java
@@ -0,0 +1,24 @@
+package com.hjjang.backend.domain.chat.domain;
+
+import com.hjjang.backend.domain.image.domain.entity.Image;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DiscriminatorValue("image")
+@Table(name = "chat_message_image")
+public class ChatMessageImage extends ChatMessage {
+
+    @OneToOne
+    @JoinColumn(name = "image_id")
+    private Image image;
+
+
+}

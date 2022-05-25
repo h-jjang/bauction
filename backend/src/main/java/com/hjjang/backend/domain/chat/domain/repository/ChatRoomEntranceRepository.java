7e3c11ca114984f79a278eb99d925adac39e0618
kim1387 <kevink0@naver.com>
2022-05-25 14:20:04 +0900

    feat: ChatRoomEntranceRepository 추가

---
+++ backend/src/main/java/com/hjjang/backend/domain/chat/domain/repository/ChatRoomEntranceRepository.java
@@ -0,0 +1,8 @@
+package com.hjjang.backend.domain.chat.domain.repository;
+
+import com.hjjang.backend.domain.chat.domain.entity.ChatRoomEntrance;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatRoomEntranceRepository extends JpaRepository<ChatRoomEntrance, Long> {
+
+}

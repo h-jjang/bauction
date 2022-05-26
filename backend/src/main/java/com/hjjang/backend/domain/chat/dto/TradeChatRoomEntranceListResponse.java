dc9c612fa5b5d157668bda0444f9d60041c378f7
kim1387 <kevink0@naver.com>
2022-05-26 10:58:31 +0900

    feat: 들어가 있는 채팅 방을 조회할 수 있는 response 제작

--- backend/src/main/java/com/hjjang/backend/domain/chat/dto/TradeChatRoomEntranceListResponse.java
+++ backend/src/main/java/com/hjjang/backend/domain/chat/dto/TradeChatRoomEntranceListResponse.java
@@ -1,17 +1,22 @@
 package com.hjjang.backend.domain.chat.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+import com.hjjang.backend.domain.chat.domain.entity.ChatRoomEntrance;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@Builder
 public class TradeChatRoomEntranceListResponse {
 
     List<TradeChatRoomEntranceInfo> tradeChatRoomEntranceInfos;
+
+
+    public TradeChatRoomEntranceListResponse(List<ChatRoomEntrance> chatRoomEntranceList) {
+        this.tradeChatRoomEntranceInfos = chatRoomEntranceList.stream()
+                .map(TradeChatRoomEntranceInfo::new)
+                .collect(Collectors.toList());
+    }
 }

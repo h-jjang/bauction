ef4e7289b82391c8de85d15f2a411e48714b985e
Gnu-Kenny <qkr0677@gmail.com>
2022-05-28 00:25:05 +0900

    refact: 게시글 등록 관련 enum 추가

--- backend/src/main/java/com/hjjang/backend/global/response/code/SuccessCode.java
+++ backend/src/main/java/com/hjjang/backend/global/response/code/SuccessCode.java
@@ -8,8 +8,12 @@ import lombok.Getter;
 public enum SuccessCode {
 
     //User
-    USER_PROFILE_SUCCESS( "U001", "프로필 조회 완료."),;
+    USER_PROFILE_SUCCESS( "U001", "프로필 조회 완료."),
 
+    //Post
+    POST_CREATE_SUCCESS( "P001", "게시글 등록 완료.")
+
+    ;
     private final String code;
     private final String message;
 }
